77072f43ded0696caac7090d6dab942029d8c9d0
soo1e <gameswuon@gmail.com>
2023-10-10 16:29:00 +0900

    최종본

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+/*eslint-disable*/
+
 import './App.css';
+import { useState } from "react";
+import Modal from "./Modal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+    let [like, setLike] = useState([0, 0, 0]);
+    let [modal, setModal] = useState(false);
+    let [mtitle, setMtitle] = useState(0);
+    let [input, setInput] = useState('');
+
+    const handleLike = (index) => {
+        let copy = [...like];
+        copy[index] = copy[index] + 1;
+        setLike(copy);
+    };
+
+    const handleDelete = (index) => {
+        let copyTitle = [...title];
+        let copyLike = [...like];
+        copyTitle.splice(index, 1);
+        copyLike.splice(index, 1);
+        setTitle(copyTitle);
+        setLike(copyLike);
+    };
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <div>Blog</div>
+            </div>
+            {
+                title.map(function (a, i) {
+                    return (
+                        <div className="list" key={i}>
+                            <h4 onClick={() => { setModal(true); setMtitle(i); }}>{title[i]}</h4>
+                            <span className="db" onClick={() => handleLike(i)}>👍 {like[i]} </span>
+                            <button onClick={() => handleDelete(i)}>삭제</button>
+                        </div>
+                    )
+                })
+            }
+
+            <input onChange={(e) => { setInput(e.target.value); }} />
+
+            <button onClick={() => {
+                let copyTitle = [...title];
+                let copyLike = [...like];
+                copyTitle.unshift(input);
+                copyLike.unshift(0);
+                setTitle(copyTitle);
+                setLike(copyLike);
+            }}>글 발행</button>
+
+            {
+                modal === true ? <Modal mtitle={mtitle} title={title} /> : null
+            }
+            {/*<SortButton title={title} setTitle={setTitle}></SortButton>*/}
+        </div>
+    )
 }
 
 export default App;
